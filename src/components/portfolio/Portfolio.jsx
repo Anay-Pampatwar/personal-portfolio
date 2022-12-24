476923ba95c06ccd9e22371eeac4723068dad915
Anay-Pampatwar <113262663+Anay-Pampatwar@users.noreply.github.com>
2022-12-24 00:40:34 -0500

    Skilles added

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -10,8 +10,8 @@ import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium'
 import CodeIcon from '@mui/icons-material/Code'
 import ThreePIcon from '@mui/icons-material/ThreeP'
 import { Typography } from '@mui/material'
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkIcon from '@mui/icons-material/Link';
+import GitHubIcon from '@mui/icons-material/GitHub'
+import LinkIcon from '@mui/icons-material/Link'
 
 //resume = portfolio
 export default function Portfolio() {
@@ -151,34 +151,37 @@ export default function Portfolio() {
                   />
                   <br />
                   <h4>
-                    <span className='expected'>Infosys Limited, India - Sept 2019 to Sept 2022 - Cloud Solution Architect</span> <br/>
+                    <span className="expected">
+                      Infosys Limited, India - Sept 2019 to Sept 2022 - Cloud
+                      Solution Architect
+                    </span>{' '}
+                    <br />
                     •Provided daily cloud support & maintained client’s cloud
                     infrastructure consisting of 50+ services spread across 7
-                    AWS accounts and 3 regions 
-                    <br/>• Innovated CloudFormation
-                    templates to provision EC2 instances, RDS, CloudWatch
-                    alarms, IAM users and roles using parameters, mappings,
-                    resources, outputs <br/>• Regulated and maintained 20+ EC2
-                    servers along with managing the user provisioning, group
-                    provisioning, user ownership of 25 users and changing file
-                    permissions on OS level on these servers <br/>• Managed the
-                    access of AWS services using IAM. Restricting the users by
-                    providing the minimum access to AWS services with the help
-                    of User groups 
+                    AWS accounts and 3 regions
+                    <br />• Innovated CloudFormation templates to provision EC2
+                    instances, RDS, CloudWatch alarms, IAM users and roles using
+                    parameters, mappings, resources, outputs <br />• Regulated
+                    and maintained 20+ EC2 servers along with managing the user
+                    provisioning, group provisioning, user ownership of 25 users
+                    and changing file permissions on OS level on these servers{' '}
+                    <br />• Managed the access of AWS services using IAM.
+                    Restricting the users by providing the minimum access to AWS
+                    services with the help of User groups
                   </h4>
-                  <br/>
-                  <br/>
-                  <br/>
-                  <br/>
-                  <br/>
-                  <br/>
-                  <br/>
+                  <br />
+                  <br />
+                  <br />
+                  <br />
+                  <br />
+                  <br />
+                  <br />
                 </h3>
 
                 <img src="https://i0.wp.com/boldblushblog.com/wp-content/uploads/2018/01/img_20171231_154252-01.jpeg?ssl=1"></img>
               </div>
             </TabPanel>
-            
+
             <TabPanel value="4">
               <div className="item">
                 <h3>
@@ -193,15 +196,30 @@ export default function Portfolio() {
                   <br />
                   <h4>
                     Stocks Portfolio Management MERN Web App.
-                    <br/> Live: <a target="_blank" href='https://stocktradeportfolio.netlify.app/'>https://stocktradeportfolio.netlify.app/ <LinkIcon/></a>
-                    <br/>
-                    <a target="_blank" href='https://github.com/Anay-Pampatwar/Stocks-Portfolio-Manager-React-App'><GitHubIcon/></a>
-                    <br/>
-                    <br/>
+                    <br /> Live:{' '}
+                    <a
+                      target="_blank"
+                      href="https://stocktradeportfolio.netlify.app/"
+                    >
+                      https://stocktradeportfolio.netlify.app/ <LinkIcon />
+                    </a>
+                    <br />
+                    <a
+                      target="_blank"
+                      href="https://github.com/Anay-Pampatwar/Stocks-Portfolio-Manager-React-App"
+                    >
+                      <GitHubIcon />
+                    </a>
+                    <br />
+                    <br />
                     Recipe Suggestor And Meal Planner JavaFX Application
-                    <a target="_blank" href='https://github.com/Anay-Pampatwar/cerealkillers/tree/master'><GitHubIcon/></a>
+                    <a
+                      target="_blank"
+                      href="https://github.com/Anay-Pampatwar/cerealkillers/tree/master"
+                    >
+                      <GitHubIcon />
+                    </a>
                   </h4>
-                  
                   <br />
                   <br />
                 </h3>
@@ -210,7 +228,8 @@ export default function Portfolio() {
               </div>
             </TabPanel>
 
-            <TabPanel value="5"><div className="item">
+            <TabPanel value="5">
+              <div className="item">
                 <h3>
                   <WorkspacePremiumIcon /> My Certification
                   <hr
@@ -220,14 +239,69 @@ export default function Portfolio() {
                       height: 1,
                     }}
                   />
-                  <br/>
+                  <br />
                   AWS Certified Solution Architect Associte
-                  <h4>Validation Number: GQJP516JBEV11QGF<br/> View my certification here<a target="_blank" href='https://www.linkedin.com/posts/anay-pampatwar28_aws-certificate-activity-6830679042228854784-sqgc?utm_source=share&utm_medium=member_desktop'><LinkIcon/></a></h4>
+                  <h4>
+                    Validation Number: GQJP516JBEV11QGF
+                    <br /> View my certification here
+                    <a
+                      target="_blank"
+                      href="https://www.linkedin.com/posts/anay-pampatwar28_aws-certificate-activity-6830679042228854784-sqgc?utm_source=share&utm_medium=member_desktop"
+                    >
+                      <LinkIcon />
+                    </a>
+                  </h4>
                 </h3>
 
                 <img src="https://thumbs.dreamstime.com/b/northeastern-university-boston-massachusetts-private-research-established-127076883.jpg"></img>
-              </div></TabPanel>
-            <TabPanel value="6">Item Five</TabPanel>
+              </div>
+            </TabPanel>
+
+            <TabPanel value="6">
+              <div className="item">
+                <h3 >
+                  <WorkspacePremiumIcon /> Skills I am proficient at:
+                  <hr
+                    style={{
+                      color: 'aliceblue',
+                      backgroundColor: 'aliceblue',
+                      height: 1,
+                    }}
+                  />
+                  <br/>
+                  <h4>
+                  <table>
+                    <tr>
+                      <th>•	Object Oriented Programming: <span font-weight="200">Java, Python, C#</span> </th>
+                    </tr>
+                    <tr>
+                      <th>•	Database: <span font-weight="200">MySQL Server, Microsoft SQL Server</span> </th>
+                    </tr>
+                    <tr>
+                      <th>•	Operating System Administration: <span font-weight="200">Linux, Shell Scripting</span> </th>
+                    </tr>
+                    <tr>
+                      <th>•	Front-end technologies: <span font-weight="200">HTML, CSS, JavaScript</span> </th>
+                    </tr>
+                    <tr>
+                      <th>•	Computer Networking: 		<span font-weight="200">TCP/IP Protocols, Servers and Routers communication</span> </th>
+                    </tr>
+                    	
+                    <tr>
+                      <th>•	Cloud Platform:  <span font-weight="200">AWS - EC2, S3, IAM, Load Balancers, CloudWatch</span> 
+                      </th>
+                    </tr>
+                  </table>
+                  
+                  </h4>
+                  <br/>
+                  <br/>
+                  <br/>
+                </h3>
+
+                <img src="https://thumbs.dreamstime.com/b/northeastern-university-boston-massachusetts-private-research-established-127076883.jpg"></img>
+              </div>
+            </TabPanel>
           </TabContext>
         </Box>
       </ul>
